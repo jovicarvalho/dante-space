980ac0b6a67300525e88bb6f02f5cf4005d52fd2
João Vitor Nunes Carvalho <joao.carvalho@e-unicred.com.br>
2024-01-05 08:48:45 -0300

    ✨feat: post page

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,8 +1,28 @@
 import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModelo from "components/PostModel";
+import ReactMarkdown from "react-markdown";
+import "./Post.css";
 
 const Post = () => {
   const params = useParams();
-  console.log(params);
-  return <h1>Oi</h1>;
+  const post = posts.find((post) => {
+    return post.id === Number(params.id);
+  });
+
+  if (!post) {
+    return <h1> Post não encontrado!</h1>;
+  }
+  return (
+    <PostModelo
+      fotoCapa={`/assets/posts/${params.id}/capa.png`}
+      titulo={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <ReactMarkdown>{post.texto}</ReactMarkdown>
+      </div>
+    </PostModelo>
+  );
 };
+
 export default Post;
